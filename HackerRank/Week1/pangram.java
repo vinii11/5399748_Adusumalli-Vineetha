14c5869530bdda861c17d1ed717610ba823c237c
vinii11 <vineethaadusumalli11@gmail.com>
2025-08-11 23:02:47 +0530

    files created

---
+++ HackerRank/Week1/pangram.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String pangrams(String s) {
+    // Write your code here
+    s=s.toLowerCase();
+    boolean[] lettersPresent=new boolean[26];
+    for(char ch:s.toCharArray()){
+        if(ch>='a' &&ch<='z'){
+            lettersPresent[ch-'a']=true;
+        }
+    }
+    for(boolean present:lettersPresent){
+        if(!present){
+            return "not pangram";
+        }
+    }
+     return "pangram";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.pangrams(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
